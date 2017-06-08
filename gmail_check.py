a17adbcf87866506879154d411fd5790ce42e334
Janit Anjaria <janit92@gmail.com>
2017-06-08 13:05:25 +0530

    code refactored with more intuitive variable names

--- gmail_check.py
+++ gmail_check.py
@@ -1,20 +1,23 @@
 import imaplib, re
+import sys
 
 
 def gmail_checker(username, password):
-    i = imaplib.IMAP4_SSL('imap.gmail.com')
+    imap_object = imaplib.IMAP4_SSL('imap.gmail.com')
     try:
-        i.login(username, password)
-        x, y = i.status('To Me', '(MESSAGES UNSEEN)')
-        messages = int(re.search('MESSAGES\s+(\d+)', y[0]).group(1))
-        unseen = int(re.search('UNSEEN\s+(\d+)', y[0]).group(1))
+        imap_object.login(username, password)
+        response_status, response_info = imap_object.status('To Me', '(MESSAGES UNSEEN)')
+        messages = int(re.search('MESSAGES\s+(\d+)', response_info[0]).group(1))
+        unseen = int(re.search('UNSEEN\s+(\d+)', response_info[0]).group(1))
         return messages, unseen
     except Exception as err:
         return False, 0
 
 
 def main():
-    messages, unseen = gmail_checker('username@gmail.com', 'password')
+    user_name = 'username@gmail.com'
+    password = 'password'
+    messages, unseen = gmail_checker(user_name, password)
     print "Total Messages: %d" % messages
     print "Unseen Messages: %d" % unseen
 
